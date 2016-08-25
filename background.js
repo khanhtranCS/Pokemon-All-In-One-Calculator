5ee47a86278700f041506634e25764a3fe4cdb2e
Khanh Duc Tran <ktran035@uw.edu>
2016-08-25 18:21:16 +0700

    fetch data successfully

--- background.js
+++ background.js
@@ -12,9 +12,33 @@ chrome.runtime.onMessage.addListener(
 		// 	});
 		// }
 		if (request.greeting == "name") {
-			alert("get here!!!");
+			//alert("get here!!!");
+			requestHTML();
 			sendResponse({msg: "great!!"});
 			//getName(sendResponse);
 		}
 	}
 );
+
+function requestHTML() {
+	var request = new XMLHttpRequest();
+	request.open('GET', "https://pokeassistant.com/main/evolver?utf8=%E2%9C%93&search_pokemon_name=Squirtle&search_cp=200&search_pokemon_id=7&locale=en&commit=Evolve", true);
+	request.onload = function () {
+		var resultHTML = request.responseText;
+		console.log(resultHTML);
+		var resultDiv = document.createElement("div");
+		resultDiv.innerHTML = resultHTML;
+		console.log(resultDiv.getElementsByClassName("table evolvetable")[0].innerHTML);
+
+	}
+	request.send();
+
+	// function alertInfo() {
+	// 	alert("current " + request.responseXML);
+	// }
+}
+
+// function alertInfo() {
+
+// 	console.log("current " + this.responseText);
+// }
